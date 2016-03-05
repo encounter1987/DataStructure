6d7e3328a93ca18cd8f39d2ce9a9e3b8392988a3
Encounter27 <pradip.phab27@gmail.com>
2016-03-05 23:39:36 +0530

    Adding utilities for linked list.

---
+++ DsUtils/List.cpp
@@ -0,0 +1,96 @@
+#include "List.h"
+
+static Node* newNode(int x)
+{
+	Node* tmp = new Node();
+	tmp-> data = x;
+	tmp-> next = NULL;
+
+	return tmp;
+}
+
+void Insert(Node** head, int x)
+{
+	if( *head == NULL)
+	{
+		*head = newNode(x);
+
+		return;
+	}
+
+	Node* tmp = *head;
+	while(tmp->next)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = newNode(x);
+}
+
+
+void Delete(Node* head, int key)
+{
+	Node* prev;
+	while(head != NULL && head->data != key)
+	{
+		prev = head;
+		head = head->next;
+	}
+
+	if(head == NULL)
+	{
+		printf("WARNING: The Key to be deleted does't exist in this list\n");
+
+		return;
+	}
+
+	prev->next = head->next;
+	delete head;
+}
+
+void Delete(Node* head, Node* node)
+{
+	Node* prev;
+	while(head != NULL && head != node)
+	{
+		prev = head;
+		head = head->next;
+	}
+
+	if(head == NULL)
+	{
+		printf("WARNING: The node to be deleted does't exist in this list\n");
+
+		return;
+	}
+
+	prev->next = head->next;
+	delete head;
+}
+
+void Reverse(Node** head)
+{
+	Node *first = *head, *second = (*head)->next, *third;
+	third = second->next;
+	second->next = first;
+	first->next = NULL;
+
+	while(third)
+	{
+		first = second;
+		second = third;
+		third = second->next;
+		second->next = first;
+	}
+
+	*head = second;
+}
+
+void Print(Node* head)
+{
+	while(head)
+	{
+		printf("[%d]->", head->data);
+		head = head->next;
+	}
+	printf("(NULL)\n");
+}
---
+++ DsUtils/List.h
@@ -0,0 +1,31 @@
+/********************************************************************
+ *          SINGLY LINKED LIST
+ * This utility provides basic operartions on singly linked list:-
+ * 1. Insert a node at nth position.
+ * 2. Delete a node contains given key.
+ * 3. Delete a given node.
+ * 4. Reverse the list.
+ * 5. Print the list items.
+ *******************************************************************/
+
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdio.h>
+#include <memory.h>
+
+using namespace std;
+
+typedef struct {
+	int data;
+	Node* next;
+}Node;
+
+void Insert(Node** head, int x);
+void Delete(Node* head, int key);
+void Delete(Node* head, Node* node);
+void Reverse(Node** head);
+void Print(Node* head);
+
+
+#endif
