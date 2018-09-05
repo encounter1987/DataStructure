c1b52f01cfd56558ff95a388876f5ff49504f16c
Pradip Acharjee <pradip.sa27@gmail.com>
2018-09-05 21:16:19 +0530

    Lowest common ancestor handled duplicate case

--- BinaryTreeProblems/LowestCommonAncestor.cpp
+++ BinaryTreeProblems/LowestCommonAncestor.cpp
@@ -35,7 +35,12 @@ bool LCA(Node *root, const int &key1, const int &key2, Node *&res)
 
     if (root->left == nullptr && root->right == nullptr &&
         (root->data == key1 || root->data == key2))
+    {
+        if (key1 == key2) // Handle the case having duplicate values
+            res = root;
+
         return true;
+    }
 
     bool L = LCA(root->left, key1, key2, res);
     bool R = LCA(root->right, key1, key2, res);
@@ -65,20 +70,20 @@ int main()
     // Approch 1
     // Assumed both keys are present in tree
     Node *r = LCA(root, 2, 5);
-    cout << "(2,5)->" << (r != nullptr ? r->data : -1) << endl;
+    cout << "(2, 5)->" << (r != nullptr ? r->data : -1) << endl;
 
     r = LCA(root, 2, 8);
     cout << "(2, 8)->" << (r != nullptr ? r->data : -1) << endl;
 
     r = LCA(root, 13, 7);
-    cout << "(13,7)->" << (r != nullptr ? r->data : -1) << endl
+    cout << "(13, 7)->" << (r != nullptr ? r->data : -1) << endl
          << endl;
 
     // Approch 2
     // Not sure keys are present in tree
     Node *res = nullptr;
     LCA(root, 2, 5, res);
-    cout << "(2,5)->" << (res != nullptr ? res->data : -1) << endl;
+    cout << "(2, 5)->" << (res != nullptr ? res->data : -1) << endl;
 
     res = nullptr;
     LCA(root, 2, 8, res);
@@ -86,11 +91,19 @@ int main()
 
     res = nullptr;
     LCA(root, 13, 7, res);
-    cout << "(13,7)->" << (res != nullptr ? res->data : -1) << endl;
+    cout << "(13 7)->" << (res != nullptr ? res->data : -1) << endl;
 
     res = nullptr;
     LCA(root, 13, 20, res);
-    cout << "(13,20)->" << (res != nullptr ? res->data : -1) << endl;
+    cout << "(13, 20)->" << (res != nullptr ? res->data : -1) << endl;
+
+    res = nullptr;
+    LCA(root, 13, 13, res);
+    cout << "(13, 13)->" << (res != nullptr ? res->data : -1) << endl;
+
+    res = nullptr;
+    LCA(root, 1, 1, res);
+    cout << "(1, 1)->" << (res != nullptr ? res->data : -1) << endl;
 
     return 0;
 }
